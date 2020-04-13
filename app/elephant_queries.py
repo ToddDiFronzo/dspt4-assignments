fb6ea5f629c4af6eb22e5b6534eb719f756106fc
Todd DiFronzo <fonze@aol.com>
2020-04-12 18:38:43 -0700

    update

--- app/elephant_queries.py
+++ app/elephant_queries.py
@@ -1,14 +1,14 @@
 # app/elephant_queries.py
 
-import psycopg2
 import os
 from dotenv import load_dotenv
+import psycopg2
 
 load_dotenv()   # loads contents of the .env file into the script's enviornment
 
 DB_NAME = os.getenv("DB_NAME")					
 DB_USER = os.getenv("DB_USER")			
-DB_PASSWORD = os.getevn("DB_PASSWORD")		
+DB_PASSWORD = os.getenv("DB_PASSWORD")		
 DB_HOST = os.getenv("DB_HOST")			
 
 ### Connect to ElephantSQL-hosted PostgreSQL
@@ -25,6 +25,6 @@ query = 'SELECT * from test_table;'
 ### Note - nothing happened yet! We need to actually *fetch* from the cursor
 
 cur.execute(query)
-results = cur.fecthone()
+results = cur.fetchone()
 print(type(results))
 print(results)
